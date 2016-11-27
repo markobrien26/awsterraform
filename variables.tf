9a5939a9a1a3619ecde7d2fb106c765a0f061c98
Mark O'Brien <20061938@mail.wit.ie>
2016-11-27 20:15:24 +0000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,80 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "eu-west-1"
+}
+
+variable "aws_access_key" {
+  default = "AKIAJBFJERBHFV73X4WQ"
+}
+
+variable "aws_secret_key" {
+  default = "myBwKb/on836/K9DHb52WqaZwHA/ZFvFlMXG+isx"
+}
+
+# Ubuntu Precise 12.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-west-1 = "ami-b1cf19c6"
+    us-east-1 = "ami-de7ab6b6"
+    us-west-1 = "ami-3f75767a"
+    us-west-2 = "ami-21f78e11"
+  }
+}
+
+
+variable "identifier" {
+  default     = "mydb-rds"
+  description = "Identifier for your DB"
+}
+
+variable "storage" {
+  default     = "10"
+  description = "Storage size in GB"
+}
+
+variable "engine" {
+  default     = "postgres"
+  description = "Engine type, example values mysql, postgres"
+}
+
+variable "engine_version" {
+  description = "Engine version"
+
+  default = {
+    mysql    = "5.6.22"
+    postgres = "9.4.1"
+  }
+}
+
+variable "instance_class" {
+  default     = "db.t2.micro"
+  description = "Instance class"
+}
+
+variable "db_name" {
+  default     = "mydb"
+  description = "db name"
+}
+
+variable "username" {
+  default     = "terraform"
+  description = "User name"
+}
+
+variable "password" {
+  description = "password, provide through your ENV variables"
+}
+
